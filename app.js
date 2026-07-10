8227e09c0d3ed71b9e8ce0b3a8f2e7fdc235a279
ori levin <mynewhouseori@gmail.com>
2026-07-10 05:05:28 +0300

    Rename pending status and align select height

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ const ownerApartmentLabels = [
 ];
 
 const MAX_AREA_PHOTOS = 3;
-const APP_VERSION = "2026.07.10.121";
+const APP_VERSION = "2026.07.10.122";
 const pendingPhotoUploads = new Map();
 const PHOTO_UPLOAD_MAX_DIMENSION = 1600;
 const PHOTO_UPLOAD_QUALITY = 0.72;
